b9faf921c58c3c36e69068fae0b91d5e030799de
Luis H. Moreno <luis@suprnation.com>
2025-08-24 17:03:02 +0200

    Allowing content from google iamges

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = withNextIntl(nextConfig)
